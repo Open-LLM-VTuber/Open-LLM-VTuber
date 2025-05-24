9fbbba808dbae287eff6227f74ee35ad500fbf98
Harry_Y <yushuhang@163.com>
2025-05-24 10:06:13 +0800

    Update comment_sync.py
    删除不必要的debug

--- upgrade_codes/comment_sync.py
+++ upgrade_codes/comment_sync.py
@@ -9,7 +9,6 @@ class CommentSynchronizer:
         self.yaml = yaml
 
     def sync(self):
-        self.logger.debug("[DEBUG] Starting comment synchronization")
 
         with open(self.default_path, "r", encoding="utf-8") as f:
             default_tree = self.yaml.load(f)
@@ -26,16 +25,12 @@ class CommentSynchronizer:
                     if hasattr(default_node, 'ca') and hasattr(user_node, 'ca'):
                         if key in default_node.ca.items:
                             user_node.ca.items[key] = default_node.ca.items[key]
-                            self.logger.debug(f"[DEBUG] Synced comment for key: {current_path}")
                     sync_comments(default_node[key], user_node[key], current_path)
 
         sync_comments(default_tree, user_tree)
 
         if hasattr(default_tree, 'ca') and hasattr(user_tree, 'ca'):
             user_tree.ca.end = default_tree.ca.end
-            self.logger.debug("[DEBUG] Synced end-of-file comments")
 
         with open(self.user_path, "w", encoding="utf-8") as f:
             self.yaml.dump(user_tree, f)
-
-        self.logger.info("Comment synchronization completed.")
